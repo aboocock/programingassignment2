fb0d9c6ce4aabe6190963655f7f48dd526584373
aboocock <alexcundall@msn.com>
2017-04-27 23:24:01 +0100

    form name

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -385,7 +385,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "mini keyboard";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
