a209c505922a6d34cbe8ba87f87ccb49fc95cf8c
aboocock <alexcundall@msn.com>
2017-04-27 23:21:33 +0100

    ui change

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -62,7 +62,7 @@
             // 
             this.multipress.AutoSize = true;
             this.multipress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.multipress.Location = new System.Drawing.Point(274, 194);
+            this.multipress.Location = new System.Drawing.Point(274, 203);
             this.multipress.Margin = new System.Windows.Forms.Padding(1);
             this.multipress.Name = "multipress";
             this.multipress.Size = new System.Drawing.Size(66, 23);
